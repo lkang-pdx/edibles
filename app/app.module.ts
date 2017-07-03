c28ceac2bbf07705b03d442739e0669de2e7b5cb
Luna Kang <luna.kang.ca@gmail.com>
2017-07-03 10:37:54 -0700

    Data binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [NativeScriptModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- platforms/ios/Edibles/app/app.component.ts
+++ platforms/ios/Edibles/app/app.component.ts
@@ -5,14 +5,26 @@ import { Component } from "@angular/core";
   template: `
     <StackLayout>
       <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
+      <TextField hint="Email Address" keyboardType="email" [(ngModel)]="email"
+          autocorrect="false" autocapitalizationType="none">
+      </TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
+      <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
+      <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
     </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+
+export class AppComponent {
+  email = "nativescriptrocks@telerik.com";
+  isLoggingIn = true;
+
+  submit() {
+    alert("You’re using: " + this.email);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
