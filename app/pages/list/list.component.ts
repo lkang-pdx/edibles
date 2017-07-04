fb9e55e8ba3b34c1a952137dec6c2c475677438f
Luna Kang <luna.kang.ca@gmail.com>
2017-07-04 10:35:30 -0700

    Animations

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -14,42 +14,47 @@ import { TextField } from "ui/text-field";
 export class ListComponent implements OnInit {
   groceryList: Array<Grocery> = [];
   grocery = "";
+  isLoading = false;
+  listLoaded = false;
   @ViewChild("groceryTextField") groceryTextField: ElementRef;
 
   constructor(private groceryListService: GroceryListService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.groceryListService.load()
       .subscribe(loadedGroceries => {
         loadedGroceries.forEach((groceryObject) => {
           this.groceryList.unshift(groceryObject);
         });
+        this.isLoading = false;
+        this.listLoaded = true;
       });
   }
 
-add() {
-  if (this.grocery.trim() === "") {
-    alert("Enter a grocery item");
-    return;
+  add() {
+    if (this.grocery.trim() === "") {
+      alert("Enter a grocery item");
+      return;
+    }
+
+    // Dismiss the keyboard
+    let textField = <TextField>this.groceryTextField.nativeElement;
+    textField.dismissSoftInput();
+
+    this.groceryListService.add(this.grocery)
+      .subscribe(
+        groceryObject => {
+          this.groceryList.unshift(groceryObject);
+          this.grocery = "";
+        },
+        () => {
+          alert({
+            message: "An error occurred while adding an item to your list.",
+            okButtonText: "OK"
+          });
+          this.grocery = "";
+        }
+      )
   }
-
-  // Dismiss the keyboard
-  let textField = <TextField>this.groceryTextField.nativeElement;
-  textField.dismissSoftInput();
-
-  this.groceryListService.add(this.grocery)
-    .subscribe(
-      groceryObject => {
-        this.groceryList.unshift(groceryObject);
-        this.grocery = "";
-      },
-      () => {
-        alert({
-          message: "An error occurred while adding an item to your list.",
-          okButtonText: "OK"
-        });
-        this.grocery = "";
-      }
-    )
-}
 }
