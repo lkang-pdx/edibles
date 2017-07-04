47e91ed13e5bb8d2af7d4ee748a237911c737684
Luna Kang <luna.kang.ca@gmail.com>
2017-07-04 16:40:34 -0700

    iOS hint colors

--- platforms/ios/Edibles/app/pages/login/login.component.ts
+++ platforms/ios/Edibles/app/pages/login/login.component.ts
@@ -5,6 +5,8 @@ import { Router } from "@angular/router";
 import { Page } from "ui/page";
 import { Color } from "color";
 import { View } from "ui/core/view";
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
 
 @Component({
   selector: "my-app",
@@ -17,6 +19,8 @@ export class LoginComponent implements OnInit {
   isLoggingIn = true;
   
   @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
   constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
@@ -57,8 +61,23 @@ export class LoginComponent implements OnInit {
         () => alert("Unfortunately we were unable to create your account.")
       );
   }
+
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
+  }
+
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
     let container = <View>this.container.nativeElement;
     container.animate({
       backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
