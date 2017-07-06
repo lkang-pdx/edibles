9e3068faaff6ed243ac5ac30a130d613b48c4ffd
Luna Kang <luna.kang.ca@gmail.com>
2017-07-05 19:34:27 -0700

    Translucent status bars

--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,8 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+import { setStatusBarColors } from "./utils/status-bar-util";
+
+setStatusBarColors();
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
--- platforms/ios/Edibles/app/utils/hint-util.ts
+++ platforms/ios/Edibles/app/utils/hint-util.ts
@@ -6,10 +6,15 @@ declare var NSDictionary: any;
 declare var NSForegroundColorAttributeName: any;
 
 export function setHintColor(args: { view: TextField, color: Color }) {
-  let dictionary = new NSDictionary(
-    [args.color.ios],
-    [NSForegroundColorAttributeName]
-  );
-  args.view.ios.attributedPlaceholder = NSAttributedString.alloc().initWithStringAttributes(
-    args.view.hint, dictionary);
+  if (args.view.android) {
+    args.view.android.setHintTextColor(args.color.android);
+  }
+  if (args.view.ios) {
+    let dictionary = new NSDictionary(
+      [args.color.ios],
+      [NSForegroundColorAttributeName]
+    );
+    args.view.ios.attributedPlaceholder = NSAttributedString.alloc().initWithStringAttributes(
+      args.view.hint, dictionary);
+  }
 }
